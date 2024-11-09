8b08fc670e38850d52251b613a9bd9672c1b8f44
Carlos Benítez <83686845+carlosBenitez0@users.noreply.github.com>
2024-11-09 13:16:06 -0600

    feat: add ejercicio 2

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,2 +1,19 @@
 <!-- Problema de Suma de Números Pares:
 Crea un script que sume todos los números pares en un array de números enteros. -->
+
+<?php
+
+function sum($numbers):int {
+    // $sum = array_sum($numbers); <--
+    $sum = 0;
+    for($i = 0; $i <= count($numbers) - 1 ; $i++){
+        $sum += $numbers[$i];
+    }
+    return $sum;
+}   
+
+$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$result = sum($numbers);
+
+echo "Suma: " . (string)$result;
+
