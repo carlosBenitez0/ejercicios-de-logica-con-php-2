c3c937dd7f5bc0aa97409f60c67ce1178aab974a
Carlos Benítez <83686845+carlosBenitez0@users.noreply.github.com>
2024-11-09 13:23:35 -0600

    feat: add ejercicio 3

--- ejercicio3.php
+++ ejercicio3.php
@@ -1,3 +1,27 @@
 <!-- Problema de Frecuencia de Caracteres:
 Implementa una función que tome una cadena de texto y devuelva un array asociativo
 que muestre la frecuencia de cada carácter en la cadena. -->
+
+<?php 
+
+function frequency($text): array{
+    $text = str_split($text);
+    $frequencies = array_count_values($text);
+    return $frequencies;
+}
+
+$result = frequency("holaaaa mmmundddo");
+
+echo '<pre>';
+foreach($result as $key => $value) {
+    if($key == " "){
+        continue;
+    }else if($value == 1){
+        echo (string)'La letra ' . $key . ' aparece ' . $value . ' vez <br/>';
+    }
+    else{
+        echo (string)'La letra ' . $key . ' aparece ' . $value . ' veces <br/>';
+    }
+    
+}
+echo '</pre>';
