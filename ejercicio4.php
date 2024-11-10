1bdc8dfff8da3b88b0908e87c7434def96344f6a
Carlos Benítez <83686845+carlosBenitez0@users.noreply.github.com>
2024-11-10 07:13:52 -0600

    feat: add ejercicio 4

--- ejercicio4.php
+++ ejercicio4.php
@@ -1,3 +1,20 @@
 <!-- Problema de Bucle Anidado:
 Escribe un programa que utilice bucles anidados para imprimir un 
 patrón de asteriscos en forma de pirámide. -->
+
+<?php
+    function piramide(){
+        $rows = 5;
+
+        for ($i = 0; $i < $rows; $i++) {
+            for ($j = 0; $j < $rows - $i - 1; $j++) {
+                echo "&nbsp;";
+            }
+            for ($k = 0; $k < (1 * $i + 1); $k++) {
+                echo "*";
+            }
+            echo "<br>";
+        }
+    }
+    piramide();
+?>
